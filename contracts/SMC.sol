f60c2eeae72f71c1f6d09f0389dff7870171daef
Gleiciane Alves <112650788+GleicianeFernandes@users.noreply.github.com>
2023-11-20 22:40:14 -0300

    Update SMC.sol
    alteração SMC

--- contracts/SMC.sol
+++ contracts/SMC.sol
@@ -1,97 +1,78 @@
-pragma solidity >=0.4.25 <0.6.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.21;
 
 contract SMC {
-  enum StatesSMC { State1}
+    enum StatesSMC { State1 }
 
-  bool[3][4] TruthTable;
-  bool[3][4] TruthTableA;
-  bool[3][4] TruthTableB;
-  uint LA1;
-  uint LA2;
-  uint LB1;
-  uint LB2;
-  bool invA;
-  bool invB;
+    bool[3][4] public TruthTable;
+    bool[3][4] public TruthTableA;
+    bool[3][4] public TruthTableB;
+    uint public LA1;
+    uint public LA2;
+    uint public LB1;
+    uint public LB2;
+    bool public invA;
+    bool public invB;
+    uint256 public storedData;
+    mapping(address => uint256) public balances;
 
-  StatesSMC myState;
-    
-  
+    StatesSMC public myState;
 
-  constructor(bool[12] memory _tt, bool[12] memory _tta) public {
-    for (uint i=0;i<4;i++) {
-       for (uint j=0;j<3;j++) {
-	   TruthTable[i][j]  = _tt[i*3+j];
-	   TruthTableA[i][j]  = _tta[i*3+j];
-       }
-    }  
-  }
+    constructor(bool[12] memory _tt, bool[12] memory _tta) {
+        for (uint i = 0; i < 4; i++) {
+            for (uint j = 0; j < 3; j++) {
+                TruthTable[i][j] = _tt[i * 3 + j];
+                TruthTableA[i][j] = _tta[i * 3 + j];
+            }
+        }
+    }
 
+    function receiveTableFromB(bool[3][4] memory _tt) public {
+        for (uint i = 0; i < 4; i++) {
+            for (uint j = 0; j < 3; j++) {
+                TruthTableB[i][j] = _tt[i][j];
+            }
+        }
+    }
 
-  function receiveTableFromB(bool[3][4] memory _tt) public {
-   for (uint i=0;i<4;i++) {
-       for (uint j=0;j<3;j++) {
-	   TruthTableB[i][j]  = _tt[i][j];
-       }
-    }  
-  }
+    function receiveLinesFromA(uint l1, uint l2) public {
+        LA1 = l1;
+        LA2 = l2;
+    }
 
-  function receiveLinesFromA(uint l1, uint l2) public {
-  	   LA1=l1;
-	   LA2=l2;
-  }
+    function receiveLinesFromB(uint l1, uint l2) public {
+        LB1 = l1;
+        LB2 = l2;
+    }
 
-  function receiveLinesFromB(uint l1, uint l2) public {
-  	   LB1=l1;
-	   LB2=l2;
-  }
+    function receiveInversionFromA(bool inv) public {
+        invA = inv;
+    }
 
-  function receiveInversionFromA(bool inv) public {
-  	   invA=inv;
-  }
+    function receiveInversionFromB(bool inv) public {
+        invB = inv;
+    }
 
-  function receiveInversionFromB(bool inv) public {
-  	   invB=inv;
-  }
+    function getLine() public view returns (uint) {
+        uint linha;
+        if ((LA1 == LB1) || (LA1 == LB2)) {
+            linha = LA1;
+        }
+        if ((LA2 == LB1) || (LA2 == LB2)) {
+            linha = LA2;
+        }
+        return linha;
+    }
 
-
-  function getLine() public returns (uint) {
-           uint linha;
-	   bool r;
-  	   if ((LA1==LB1) || (LA1==LB2)) {
-	      linha=LA1;
-	   }
-	   if ((LA2==LB1) || (LA2==LB2)) {
-	      linha=LA2;
-	   }
-	   return linha;
-  }
-
-
-
-  function getValue() public returns (bool) {
-  	   uint linha;
-	   bool r;
-  	   if ((LA1==LB1) || (LA1==LB2)) {
-	      linha=LA1;
-	   }
-	   if ((LA2==LB1) || (LA2==LB2)) {
-	      linha=LA2;
-	   }
-	   r = TruthTableB[linha][2];
-	   return (r != invA) != invB;
-  }
-  
-
-  // gets TT uploaded by A
-  function getTTA() public returns (bool[3][4] memory) {
-	   return TruthTableA;
-  }
-
-  //get TT uploaded by B
-  function getTTB() public returns (bool[3][4] memory) {
-	   return TruthTableB;
-  }
-
-  
-  
+    function getValue() public view returns (bool) {
+        uint linha;
+        if ((LA1 == LB1) || (LA1 == LB2)) {
+            linha = LA1;
+        }
+        if ((LA2 == LB1) || (LA2 == LB2)) {
+            linha = LA2;
+        }
+        bool r = TruthTableB[linha][2];
+        return (r != invA) != invB;
+    }
 }
